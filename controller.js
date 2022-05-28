db0281ee9881aa7406066f1270369b50555450fd
Mahmud <simply.mahmud@gmail.com>
2022-05-28 22:45:40 +0600

    CRUD operations using AngularJS

---
+++ controller.js
@@ -0,0 +1,26 @@
+var app = angular.module("myTodoList", []);
+
+app.controller("myCtrl", function($scope){
+
+    $scope.allTodos = ["xor bite","sleep","office","coding"];
+
+    $scope.add = function(){
+        $scope.allTodos.push($scope.addTodo);
+        $scope.addTodo = "";
+    }
+
+    $scope.delete =  function(x){
+        $scope.allTodos.splice(x,1);
+    }
+
+    $scope.editingData = function(x,index){
+        $scope.addTodo = x;
+        //console.log("test "+index);
+        $scope.UpdateIndex = index;
+    }
+
+    $scope.update = function(){
+        //console.log("test2 "+$scope.UpdateIndex);
+        $scope.allTodos[$scope.UpdateIndex]=$scope.addTodo;
+    } 
+});
